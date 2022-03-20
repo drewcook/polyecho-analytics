2c20bae13e0d77003ca4c110807e9ad22f17a32c
Drew Cook <drewcook29@gmail.com>
2022-03-20 13:37:16 -0600

    chore: Update config for Kovan pref

--- src/config.js
+++ src/config.js
@@ -1,31 +1,33 @@
 export const CONFIG = {
 	TEMPLATE: {
-		// Kovan contract address
-		collection_address: '0xf6Cc7488c5b6Ce204089872AFb46aE957b563F7D',
+		// Kovan contract address - https://kovan.etherscan.io/address/0xaeca10e3d2db048db77d8c3f86a9b013b0741ba2
+		collection_address: '0xaeca10e3d2db048db77d8c3f86a9b013b0741ba2',
+
 		// Note: Class A endpoints (NFTs) are not supported on Polygon, but they are on Arbitrum
 		block_chain_id: '42', // Kovan
+
 		// Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
 		title: 'PolyEchoNFT Collection',
+
 		// Display the floor price chart
 		timeseries_chart: true,
+
 		// Set the banner image to PolyEchoNFT Default Image from NFT.storage
 		banner_picture:
 			'https://bafkreia7jo3bjr2mirr5h2okf5cjsgg6zkz7znhdboyikchoe6btqyy32u.ipfs.dweb.link/',
 	},
 
-	// Supported networks - just for your reference
+	// Supported networks
 	FILTER_OPTIONS: [
 		{ name: 'Ethereum', value: 1 },
+		{ name: 'Kovan', value: 42 },
+		{ name: 'Rinkeby', value: 4 },
 		{ name: 'Polygon', value: 137 },
 		{ name: 'Polygon Mumbai', value: 80001 },
-		{ name: 'Avalanche', value: 43114 },
-		{ name: 'Fantom', value: 250 },
-		{ name: 'Moonbeam', value: 1284 },
-		{ name: 'Moonriver', value: 1285 },
 		{ name: 'Arbitrum', value: 42161 },
-		{ name: 'Shiden', value: 336 },
+		{ name: 'Arbitrum Rinkeby', value: 421611 },
 	],
-	// Supported timeframes - just for your reference
+	// Supported timeframes
 	GRAPH_OPTIONS: [
 		{ name: '7 Days', value: 7 },
 		{ name: '1 Month', value: 30 },
